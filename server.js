a66e05d9651d1ce62815888e1073eeaadf3f50e2
mushtaq <mushtaq.safie@students.mq.edu.au>
2021-02-18 13:52:19 +1100

    server api

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uniqid = require('uniqid');
 
 const app = express();
 const PORT = 8080;
@@ -14,12 +16,28 @@ app.use(express.json());
 //   }
 // ];
 
-console.log(obj[0].title);
+// console.log(obj[0].title);
 
 app.use(express.static(path.join(__dirname, 'public/assets')))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req, res) => {
+  // var obj = JSON.parse(fs.readFileSync('file', 'utf8'));
+  // fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8')
+  res.json(JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8')));
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.ID = uniqid();
+  const obj = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8'))
+  obj.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(obj, null, 2))
+  res.json(JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json'), 'utf-8')));
+  console.log(newNote);
+});
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
